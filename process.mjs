d2cb1064cb05a5fb8e764b0528536179008abda0
Thomzes <thomz161@gmail.com>
2023-02-26 01:24:06 +0700

    process modules

---
+++ process.mjs
@@ -0,0 +1,14 @@
+import process, { exitCode } from "process";
+
+process.addListener("exit", (exitCode) => {
+  console.info(`NodeJS exit with code ${exitCode}`);
+});
+
+console.info(process.version);
+console.table(process.argv);
+console.table(process.report);
+console.table(process.env);
+
+process.exit(1);
+
+console.info("Note Printed because already exit");
